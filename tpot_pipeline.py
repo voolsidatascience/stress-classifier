6b63a56d2fc09afabc05beeb4c3890998fdfd4d9
Phuong Nguyen <ngthianhphuong@gmail.com>
2020-08-29 18:07:25 +0200

    simulation code using AutoML

--- tpot_pipeline.py
+++ tpot_pipeline.py
@@ -1,15 +1,22 @@
 import numpy as np
-
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 
-# NOTE: Make sure that the class is labeled 'class' in the data file
-tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
-features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
-    train_test_split(features, tpot_data['class'], random_state=42)
+            train_test_split(features, tpot_data['target'], random_state=None)
 
-exported_pipeline = KNeighborsClassifier(n_neighbors=4, weights="distance")
+# Average CV score on the training set was: 0.8755676657584015
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=BernoulliNB(alpha=0.001, fit_prior=False)),
+    GradientBoostingClassifier(learning_rate=0.1, max_depth=10, max_features=1.0, min_samples_leaf=7, min_samples_split=12, n_estimators=100, subsample=0.6000000000000001)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
